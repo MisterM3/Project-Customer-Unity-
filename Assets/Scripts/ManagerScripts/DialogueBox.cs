a0445fb84160fa407657bdca395342390f732a7d
EnjoyW0rld <denisavatar@hotmail.com>
2022-09-14 15:47:56 +0200

    Dialogue box fix

--- Assets/Scripts/ManagerScripts/DialogueBox.cs
+++ Assets/Scripts/ManagerScripts/DialogueBox.cs
@@ -9,6 +9,8 @@ public class DialogueBox : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI _textMeshPro;
     [SerializeField] int CharactersPerSecond;
+    [SerializeField] int timeToDissapear;
+    float timeLeft;
     IEnumerator WriteTextCoroutine;
     char[] letters;
 
@@ -25,17 +27,19 @@ public class DialogueBox : MonoBehaviour
         Instance = this;
         WriteTextCoroutine = WriteText();
     }
-
-    public void Start()
-    {
-        //SetDialogue("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-    }
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !isTextPrinted)
         {
             SkipText();
+        }
+        if (isTextPrinted)
+        {
+            timeLeft -= Time.deltaTime;
+            if(timeLeft < 0)
+            {
+                _textMeshPro.text = " ";
+            }
         }
     }
 
@@ -48,9 +52,11 @@ public class DialogueBox : MonoBehaviour
         StopCoroutine("WriteText");
         _textMeshPro.text = letters.ArrayToString();
         isTextPrinted = true;
+        timeLeft = timeToDissapear;
 
     }
 
+    
     /// <summary>
     /// Coroutine function to print text letter by letter
     /// </summary>
@@ -64,6 +70,7 @@ public class DialogueBox : MonoBehaviour
             yield return new WaitForSeconds(1f / CharactersPerSecond);
         }
         isTextPrinted = true;
+        timeLeft = timeToDissapear;
     }
 
     /// <summary>
