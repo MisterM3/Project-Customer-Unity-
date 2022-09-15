38074e2ce4ccf827cae327b1f1af19835af765f8
MisterM <510249@student.saxion.nl>
2022-09-15 08:34:04 +0200

    Fixed the gradualDying script

---
+++ Assets/MennoTestGround/Scripts/GradualDyingPostProcessing.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class GradualDyingPostProcessing : MonoBehaviour
+{
+    [SerializeField] private Volume deathVolume;
+
+    Vignette vignette;
+
+    Vignette currentStageVignette;
+
+    [SerializeField] List<VolumeProfile> stagesOfDying = new List<VolumeProfile>();
+    private int stages = 0;
+    private void Start()
+    {
+        deathVolume.profile.TryGet<Vignette>(out vignette);
+        NewEffectsStage();
+    }
+
+    private void Update()
+    {
+        if (vignette.intensity.value < currentStageVignette.intensity.value) vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, currentStageVignette.intensity.value, Time.deltaTime / 2f);
+    }
+
+
+    private void NewEffectsStage()
+    {
+        stagesOfDying[stages].TryGet<Vignette>(out currentStageVignette);
+    }
+}
