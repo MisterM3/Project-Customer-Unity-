d68dd30477d19c2982a46c188e0b0319c591a6b9
MisterM <510249@student.saxion.nl>
2022-09-15 11:14:46 +0200

    Added gradual dying per stage

--- Assets/MennoTestGround/Scripts/GradualDyingPostProcessing.cs
+++ Assets/MennoTestGround/Scripts/GradualDyingPostProcessing.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,28 +7,82 @@ using UnityEngine.Rendering.Universal;
 
 public class GradualDyingPostProcessing : MonoBehaviour
 {
+
+     float intensityVignette;
+     float intensityGrain;
+     float responseGrain;
+     float saturationColorAdjustments;
+
+
+
     [SerializeField] private Volume deathVolume;
 
     Vignette vignette;
+    FilmGrain grain;
+    ColorAdjustments colorAdjustments;
 
     Vignette currentStageVignette;
+    FilmGrain currentStageGrain;
+    ColorAdjustments currentStageColorAdjustments;
 
-    [SerializeField] List<VolumeProfile> stagesOfDying = new List<VolumeProfile>();
-    private int stages = 0;
+    [SerializeField] VolumeProfile effectJustBeforeDying;
+
+    private int stage = 8;
+    [SerializeField] private int totalStages = 8;
     private void Start()
     {
-        deathVolume.profile.TryGet<Vignette>(out vignette);
+        if (deathVolume == null)
+        {
+            Debug.LogError("No Volume selected to deathVolume: " + gameObject);
+            return;
+        }
+
+        if (effectJustBeforeDying == null)
+        {
+            Debug.LogError("No Volume selected to effectJustBeforeDying: " + gameObject);
+            return;
+        }
+
+        Setup();
         NewEffectsStage();
     }
 
+    //
     private void Update()
     {
-        if (vignette.intensity.value < currentStageVignette.intensity.value) vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, currentStageVignette.intensity.value, Time.deltaTime / 2f);
+        float timeForLerp = Time.deltaTime / 2.0f;
+        if (vignette.intensity.value - intensityVignette < 0.1f) vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, intensityVignette, timeForLerp);
+        if (grain.intensity.value - intensityGrain < 0.1f) grain.intensity.value = Mathf.Lerp(grain.intensity.value, intensityGrain, timeForLerp);
+        if (grain.response.value - responseGrain < 0.1f) grain.response.value = Mathf.Lerp(grain.response.value, responseGrain, timeForLerp);
+        if (colorAdjustments.saturation.value - saturationColorAdjustments > 0.1f) colorAdjustments.saturation.value = Mathf.Lerp(colorAdjustments.saturation.value, saturationColorAdjustments, timeForLerp);
+        Debug.Log(colorAdjustments.saturation.value);
     }
 
+    private void Setup()
+    {
+        deathVolume.profile.TryGet<Vignette>(out vignette);
+        deathVolume.profile.TryGet<FilmGrain>(out grain);
+        deathVolume.profile.TryGet<ColorAdjustments>(out colorAdjustments);
 
+        effectJustBeforeDying.TryGet<Vignette>(out currentStageVignette);
+        effectJustBeforeDying.TryGet<FilmGrain>(out currentStageGrain);
+        effectJustBeforeDying.TryGet<ColorAdjustments>(out currentStageColorAdjustments);
+    }
     private void NewEffectsStage()
     {
-        stagesOfDying[stages].TryGet<Vignette>(out currentStageVignette);
+        intensityVignette = currentStageVignette.intensity.value / totalStages * stage;
+        intensityGrain = currentStageGrain.intensity.value / totalStages * stage;
+        responseGrain = currentStageGrain.response.value / totalStages * stage;
+        saturationColorAdjustments = currentStageColorAdjustments.saturation.value / totalStages * stage;
+
+    }
+
+    public void nextStage()
+    {
+        stage++;
+        NewEffectsStage();
     }
 }
+
+
+
