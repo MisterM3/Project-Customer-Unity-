c5625bfdcfde7e9e02bd21a355deec750dcce74f
MisterM <510249@student.saxion.nl>
2022-09-20 15:34:03 +0200

    Added dialogue to the objectives

--- Assets/Scripts/ObjectiveTriggers.cs
+++ Assets/Scripts/ObjectiveTriggers.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class ObjectiveTriggers : MonoBehaviour
 {
 
+    private bool once = true;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("AAAAAAAAAAAAAAA");
         ObjectiveScene.Instance.IsTriggerTheObjective(this.GetComponent<Collider>());
+
+        if (once && TryGetComponent<FuncExetion>(out FuncExetion funcEx))
+        {
+            funcEx.Interact();
+            once = false;
+        }
+        
     }
 }
